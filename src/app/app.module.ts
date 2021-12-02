41d553574e1cb362479ce51aef795cff8aca53f5
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2021-12-02 04:20:42 -0700

    Add note-detail and note-edit components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
 import { NoteGridComponent } from './notes/note-grid/note-grid.component';
+import { NoteDetailComponent } from './notes/note-detail/note-detail.component';
+import { NoteEditComponent } from './notes/note-edit/note-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesComponent,
-    NoteGridComponent
+    NoteGridComponent,
+    NoteDetailComponent,
+    NoteEditComponent
   ],
   imports: [
     BrowserModule
